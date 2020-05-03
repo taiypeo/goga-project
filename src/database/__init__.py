e5024e7857e019d3453312d9e9ca4368b94df63e
QwertygidQ <Qwertygid@gmail.com>
2020-05-03 14:46:30 +0300

    Initial DB commit

---
+++ src/database/__init__.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine("sqlite:///../db.sqlite")
+Base = declarative_base()
+
+from .user import *
+
+Base.metadata.create_all(engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
