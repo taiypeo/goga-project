c62033f360e9ce875b4b6aff6e6ceb2f49518a4f
syn <isaqtm@gmail.com>
2020-05-04 11:18:03 +0300

    Extract tests

--- src/main.py
+++ src/main.py
@@ -13,32 +13,6 @@ from src.database.api import permission_courses, add_token, check_token_presence
 logger = logging.getLogger()
 logger.setLevel(level=os.environ.get("LOGLEVEL", "ERROR").upper())
 
-
-def test_db():
-    from database import session, User, Event
-    from datetime import datetime, timedelta
-    import random
-
-    u = User(telegram_id=str(int(time.time())))
-    print(u)
-
-    session.add(u)
-    for _ in range(7):
-        e = Event(
-            date=datetime.now() +
-            timedelta(milliseconds=random.randint(1, 1000))
-        )
-        session.add(e)
-        u.events.append(e)
-    session.commit()
-
-    print(*Event.upcoming_events(session, 2), sep="\n")
-
-
-test_db()
-time.sleep(2) #  wait for scheduler to do jobs
-
-
 if "TG_BOT_TOKEN" not in os.environ:
     logger.critical("No TG_BOT_TOKEN environment variable found")
     sys.exit(1)
---
+++ src/test.py
@@ -0,0 +1,29 @@
+from database import session, User, Event
+from datetime import datetime, timedelta
+import random
+import time
+import logging
+import os
+
+logger = logging.getLogger()
+logger.setLevel(level=os.environ.get("LOGLEVEL", "ERROR").upper())
+
+def test_db():
+    u = User(telegram_id=str(int(time.time())))
+    print(u)
+
+    session.add(u)
+    for _ in range(7):
+        e = Event(
+            date=datetime.now() +
+            timedelta(milliseconds=random.randint(1, 1000))
+        )
+        session.add(e)
+        u.events.append(e)
+    session.commit()
+
+    print(*Event.upcoming_events(session, 2), sep="\n")
+
+
+test_db()
+time.sleep(2) #  wait for scheduler to do jobs
