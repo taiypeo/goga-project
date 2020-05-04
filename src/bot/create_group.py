9c16a54b780970cacf35eefd8ca98952fe715f1b
syn <isaqtm@gmail.com>
2020-05-04 22:00:13 +0300

    Improve some shit

--- src/bot/create_group.py
+++ src/bot/create_group.py
@@ -1,59 +1,41 @@
 from . import dispatcher
-from ..database import Session, User, Group, Permission, Perm, add_to_database
+from ..database import thread_local_session, User, Group, Permission, Perm, add_to_database
 from telegram.ext import CommandHandler
 
 
 def create_group(update, context):
-    session = Session()
-
     tg_id = update.effective_user.id
-    user = session.query(User).filter_by(tg_id=tg_id).first()
-    if user is None:
-        context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            text=(
+
+    def reply(msg):
+        context.bot.send_message(chat_id=update.effective_chat.id, text=(msg))
+
+    with thread_local_session() as session:
+        user = session.query(User).filter_by(tg_id=tg_id).first()
+        if user is None:
+            return reply(
                 "Пользователь не найден в системе. "
                 "Попробуйте воспользоваться командой '/start'."
-            ),
-        )
-        Session.remove()
-        return
+            )
 
-    if context.args is None or context.args == []:
-        context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            text="Название группы не может быть пустым.",
-        )
-        Session.remove()
-        return
+        if context.args is None or context.args == []:
+            return reply("Название группы не может быть пустым.")
 
-    group_title = " ".join(context.args)
-    if group_title == "":
-        context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            text="Название группы не может быть пустым.",
-        )
-        Session.remove()
-        return
+        group_title = " ".join(context.args)
+        if group_title == "":
+            return reply("Название группы не может быть пустым.")
 
-    group = Group(title=group_title)
+        group = Group(title=group_title)
 
-    perm = Permission(group=group, user=user, perm=Perm.all())
+        perm = Permission(group=group, user=user, perm=Perm.all())
 
-    if not add_to_database([group, perm], session):
-        context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            text=(
-                "Не удалось создать группу. Попробуйте еще "
-                "раз или обратитесь к администратору."
-            ),
-        )
-    else:
-        context.bot.send_message(
-            chat_id=update.effective_chat.id, text="Группа успешно создана!"
-        )
+        if not add_to_database([group, perm], session):
+            return reply(
+                    "Не удалось создать группу. Попробуйте еще "
+                    "раз или обратитесь к администратору."
+                ),
+        else:
+            return reply("Группа успешно создана!")
 
-    Session.remove()
 
 
 create_group_handler = CommandHandler("create_group", create_group)
--- src/database/utils.py
+++ src/database/utils.py
@@ -1,4 +1,15 @@
 from typing import List
+from sqlalchemy.exc import DatabaseError
+from . import Session
+from contextlib import contextmanager
+
+
+@contextmanager
+def thread_local_session():
+    try:
+        yield Session()
+    finally:
+        Session.remove()
 
 
 def add_to_database(obj_list: List[object], session) -> bool:
